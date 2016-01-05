61df437244de043d6c7d1130be6eff6e6f107752
Maciek Swat <maciekswat@gmail.com>
2016-01-05 10:50:03 -0500

    added raw eeg read tests

--- ptsa/tests/test_eeg_read.py
+++ ptsa/tests/test_eeg_read.py
@@ -1,80 +1,96 @@
 __author__ = 'm'
 
 
-from ptsa.data.readers import BaseEventReader
-from ptsa.data.readers import PTSAEventReader
-from ptsa.data.events import Events
 
 import numpy as np
-# from numpy.testing import *
+from numpy.testing import *
 import unittest
-from unittest import *
+# from unittest import *
+
+from EventReadersTestBase import EventReadersTestBase
+from ptsa.data.events import Events
 
 
-class TestEventRead(unittest.TestCase):
+class TestEEGRead(unittest.TestCase,EventReadersTestBase):
+
     def setUp(self):
+
         self.event_range = range(0,30,1)
         self.e_path = '/Users/m/data/events/RAM_FR1/R1060M_events.mat'
 
 
-    def read_ptsa_events(self):
 
-        e_reader = PTSAEventReader(event_file=self.e_path, eliminate_events_with_no_eeg=True)
-        e_reader.read()
+    def test_eeg_read(self):
 
-        events = e_reader.get_output()
-
-        events = events[events.type == 'WORD']
-
-        events = events[self.event_range]
-
-        ev_order = np.argsort(events, order=('session','list','mstime'))
-        events = events[ev_order]
-        return events
-
-
-    def read_base_events(self):
-
-        base_e_reader = BaseEventReader(event_file=self.e_path, eliminate_events_with_no_eeg=True, use_ptsa_events_class=False)
-
-
-        base_e_reader.read()
-
-        base_events = base_e_reader.get_output()
-
-        base_events = base_events[base_events.type == 'WORD']
-
-        base_ev_order = np.argsort(base_events, order=('session','list','mstime'))
-        base_events = base_events[base_ev_order]
-
-        base_events = base_events[self.event_range]
-
-        return base_events
-
-
-    def test_event_read(self):
-        # self.initialize()
 
         events = self.read_ptsa_events()
+
+        # in case fancy indexing looses Eventness of events we need to create Events object explicitely
+        if not isinstance(events,Events):
+            events = Events(events)
+
         base_events = self.read_base_events()
 
+        eegs= events.get_data(channels=['002','003'], start_time=0.0, end_time=1.6,
+                              buffer_time=1.0, eoffset='eegoffset', keep_buffer=False,
+                              eoffset_in_time=False,verbose=True)
 
-        for base_event, event in zip(base_events,events):
-            self.assertEqual(base_event['item'],event['item'])
 
-        for base_event, event in zip(base_events,events):
-            self.assertEqual(base_event.eegoffset,event.eegoffset)
 
-    @unittest.expectedFailure
-    def test_eventness(self):
-        # self.initialize()
+
+        from ptsa.data.readers.TimeSeriesEEGReader import TimeSeriesEEGReader
+
+        time_series_reader = TimeSeriesEEGReader(base_events)
+
+        time_series_reader.start_time = 0.0
+        time_series_reader.end_time = 1.6
+        time_series_reader.buffer_time = 1.0
+        time_series_reader.keep_buffer = False
+
+        time_series_reader.read(channels=['002','003'])
+
+
+        base_eegs = time_series_reader.get_output()
+
+        assert_array_equal(eegs,base_eegs.data)
+
+
+    def test_eeg_read_keep_buffer(self):
+
+
         events = self.read_ptsa_events()
-        self.assertIsInstance(events,Events,"WARNING:Warning Fancy Indexing Causes Events to be recarray")
 
-        # assert_warns(UserWarning('Warning Fancy Indexing Causes Events to be recarray'), isinstance, events, Events)
-        # # if not isinstance(events,Events):
-        # #     print 'GOT NON EVENTS'
-        # # # assert_raises(RuntimeError, isinstance , events, Events)
+        # in case fancy indexing looses Eventness of events we need to create Events object explicitely
+        if not isinstance(events,Events):
+            events = Events(events)
+
+        base_events = self.read_base_events()
+
+        eegs= events.get_data(channels=['002','003'], start_time=0.0, end_time=1.6,
+                              buffer_time=1.0, eoffset='eegoffset', keep_buffer=True,
+                              eoffset_in_time=False,verbose=True)
+
+
+
+
+        from ptsa.data.readers.TimeSeriesEEGReader import TimeSeriesEEGReader
+
+        time_series_reader = TimeSeriesEEGReader(base_events)
+
+        time_series_reader.start_time = 0.0
+        time_series_reader.end_time = 1.6
+        time_series_reader.buffer_time = 1.0
+        time_series_reader.keep_buffer = True
+
+        time_series_reader.read(channels=['002','003'])
+
+
+        base_eegs = time_series_reader.get_output()
+
+        assert_array_equal(eegs,base_eegs.data)
+
+
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
--- ptsa/tests/test_suite.py
+++ ptsa/tests/test_suite.py
@@ -2,11 +2,14 @@ __author__ = 'm'
 
 import unittest
 from test_event_read import TestEventRead
+from test_eeg_read import TestEEGRead
 # from test_wavelet import test_morlet_multi
 
 if __name__=='__main__':
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(TestEventRead))
+    test_suite.addTest(unittest.makeSuite(TestEEGRead))
+
     # test_suite.addTest(unittest.makeSuite(test_morlet_multi))
 
     runner=unittest.TextTestRunner()
