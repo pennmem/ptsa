f2219d0e63ecda8e652f447f1a2bd8418e56bf4b
Maciek Swat <maciekswat@gmail.com>
2016-01-13 20:47:07 -0500

    Adder read/write test to TimeSeriesXrayTest

--- ptsa/tests/TimeSeriesXrayTest.py
+++ ptsa/tests/TimeSeriesXrayTest.py
@@ -2,6 +2,7 @@ __author__ = 'm'
 
 import xray
 import numpy as np
+from ptsa.data.TimeSeriesXray import TimeSeriesXray
 import pathlib
 
 from os.path import *
@@ -34,158 +35,6 @@ def attach_rawbinwrappers(evs):
 
 
 
-class TimeSeriesXray(xray.DataArray):
-
-    def __init__(self,data,**kwds):
-        xray.DataArray.__init__(self,data,**kwds)
-        self.a=10
-        self.time_axis_index=get_axis_index(self,axis_name='time')
-
-
-
-    def filtered(self,freq_range,filt_type='stop',order=4):
-        """
-        Filter the data using a Butterworth filter and return a new
-        TimeSeries instance.
-
-        Parameters
-        ----------
-        freq_range : {array_like}
-            The range of frequencies to filter.
-        filt_type = {scipy.signal.band_dict.keys()},optional
-            Filter type.
-        order = {int}
-            The order of the filter.
-
-        Returns
-        -------
-        ts : {TimeSeries}
-            A TimeSeries instance with the filtered data.
-        """
-
-        from ptsa.filt  import buttfilt
-
-        filtered_array = buttfilt(self.values, freq_range, self.attrs['samplerate'], filt_type,
-                                       order,axis=self.time_axis_index)
-
-        # filtered_time_series = xray.DataArray(
-        #     filtered_array,
-        #     coords = [xray.DataArray(coord.copy()) for coord_name, coord in self.coords.items() ]
-        # )
-
-        filtered_time_series = TimeSeriesXray(
-            filtered_array,
-            coords = [xray.DataArray(coord.copy()) for coord_name, coord in self.coords.items() ]
-        )
-
-
-        filtered_time_series.attrs = self.attrs.copy()
-
-        return filtered_time_series
-
-
-    def resampled(self, resampled_rate, window=None,
-                  loop_axis=None, num_mp_procs=0, pad_to_pow2=False):
-        '''
-
-        :param resampled_rate: resample rate
-        :param window: ignored for now - added for legacy reasons
-        :param loop_axis: ignored for now - added for legacy reasons
-        :param num_mp_procs: ignored for now - added for legacy reasons
-        :param pad_to_pow2: ignored for now - added for legacy reasons
-        :return: resampled time series
-        '''
-
-        from scipy.signal import resample
-        samplerate = self.attrs['samplerate']
-
-
-        time_axis = self['time']
-        time_axis_length = np.squeeze(time_axis.shape)
-        new_length = int(np.round(time_axis_length*resampled_rate/float(samplerate)))
-
-        # print new_length
-
-        # if self.time_axis_index<0:
-        #     self.time_axis_index = get_axis_index(data_array=self, axis_name='time')
-
-        # time_axis = self.coords[ self.dims[self.time_axis_index] ]
-
-        # time_axis = self['time']
-
-        resampled_array, new_time_axis = resample(self.values,
-                                         new_length, t=time_axis.values,
-                                         axis=self.time_axis_index, window=window)
-
-
-        # print new_time_axis
-
-        #constructing axes
-        coords = []
-        for i, dim_name in enumerate(self.dims):
-            if i != self.time_axis_index:
-                coords.append(self.coords[dim_name].copy())
-            else:
-                coords.append((dim_name,new_time_axis))
-
-
-        resampled_time_series = xray.DataArray(resampled_array, coords=coords)
-        resampled_time_series.attrs['samplerate'] = resampled_rate
-
-        return resampled_time_series
-
-
-    def remove_buffer(self, duration):
-	"""
-        Remove the desired buffer duration (in seconds) and reset the
-	time range.
-
-        Parameter
-        ---------
-        duration : {int,float,({int,float},{int,float})}
-            The duration to be removed. The units depend on the samplerate:
-            E.g., if samplerate is specified in Hz (i.e., samples per second),
-            the duration needs to be specified in seconds and if samplerate is
-            specified in kHz (i.e., samples per millisecond), the duration needs
-            to be specified in milliseconds.
-            A single number causes the specified duration to be removed from the
-            beginning and end. A 2-tuple can be passed in to specify different
-            durations to be removed from the beginning and the end respectively.
-
-        Returns
-        -------
-        ts : {TimeSeries}
-            A TimeSeries instance with the requested durations removed from the
-            beginning and/or end.
-        """
-
-        number_of_buffer_samples =  int(np.ceil(duration*self.attrs['samplerate']))
-        if number_of_buffer_samples > 0:
-            return  self[:,:,number_of_buffer_samples:-number_of_buffer_samples]
-
-
-
-    def baseline_corrected(self, base_range):
-        """
-
-        Return a baseline corrected timeseries by subtracting the
-        average value in the baseline range from all other time points
-        for each dimension.
-
-        Parameters
-        ----------
-        base_range: {tuple}
-            Tuple specifying the start and end time range (inclusive)
-            for the baseline.
-
-        Returns
-        -------
-        ts : {TimeSeries}
-            A TimeSeries instance with the baseline corrected data.
-
-        """
-
-        return self - self.isel(time=(self['time'] >= base_range[0]) & (self['time'] <= base_range[1])).mean(dim='time')
 
 
 d = np.arange(120).reshape(4,3,10)
@@ -233,6 +82,35 @@ corrected_base_ev_data_xray = base_ev_data_xray.baseline_corrected((0.0,0.2))
 # remove buffer test
 no_buffer_base_ev_data_ptsa = base_ev_data_ptsa.remove_buffer(1.0)
 no_buffer_base_ev_data_xray = base_ev_data_xray.remove_buffer(1.0)
+
+
+no_buffer_dataset = xray.Dataset({'no_buffer':no_buffer_base_ev_data_xray})
+
+
+from ptsa.data.writers.NetCDF4XrayWriter import NetCDF4XrayWriter
+nc4_writer = NetCDF4XrayWriter(no_buffer_base_ev_data_xray)
+nc4_writer.write('no_buffer_base_ev_data_xray_new.nc')
+
+
+from ptsa.data.readers.NetCDF4XrayReader import NetCDF4XrayReader
+nc4_reader = NetCDF4XrayReader()
+array = nc4_reader.read('no_buffer_base_ev_data_xray_new.nc')
+
+print array['events']
+
+
+sys.exit()
+
+a = no_buffer_base_ev_data_xray
+a['events']=np.arange(len(a['events']))
+a['channels']=np.arange(len(a['channels']))
+# a['channels']=np.arange(len(a['channels']))
+# a['events']=np.arange(len(a['events']))
+print a
+nb = xray.Dataset({'no_buffer':a})
+nb.to_netcdf('no_buffer.nc')
+
+
 
 
 print 'EXITING AFTER BUFFER TEST'
@@ -290,3 +168,160 @@ print
 
 
 # print  'ts=',ts
+
+
+
+
+
+# class TimeSeriesXray(xray.DataArray):
+#
+#     def __init__(self,data,**kwds):
+#         xray.DataArray.__init__(self,data,**kwds)
+#         self.a=10
+#         self.time_axis_index=get_axis_index(self,axis_name='time')
+#
+#
+#
+#     def filtered(self,freq_range,filt_type='stop',order=4):
+#         """
+#         Filter the data using a Butterworth filter and return a new
+#         TimeSeries instance.
+#
+#         Parameters
+#         ----------
+#         freq_range : {array_like}
+#             The range of frequencies to filter.
+#         filt_type = {scipy.signal.band_dict.keys()},optional
+#             Filter type.
+#         order = {int}
+#             The order of the filter.
+#
+#         Returns
+#         -------
+#         ts : {TimeSeries}
+#             A TimeSeries instance with the filtered data.
+#         """
+#
+#         from ptsa.filt  import buttfilt
+#
+#         filtered_array = buttfilt(self.values, freq_range, self.attrs['samplerate'], filt_type,
+#                                        order,axis=self.time_axis_index)
+#
+#         # filtered_time_series = xray.DataArray(
+#         #     filtered_array,
+#         #     coords = [xray.DataArray(coord.copy()) for coord_name, coord in self.coords.items() ]
+#         # )
+#
+#         filtered_time_series = TimeSeriesXray(
+#             filtered_array,
+#             coords = [xray.DataArray(coord.copy()) for coord_name, coord in self.coords.items() ]
+#         )
+#
+#
+#         filtered_time_series.attrs = self.attrs.copy()
+#
+#         return filtered_time_series
+#
+#
+#     def resampled(self, resampled_rate, window=None,
+#                   loop_axis=None, num_mp_procs=0, pad_to_pow2=False):
+#         '''
+#
+#         :param resampled_rate: resample rate
+#         :param window: ignored for now - added for legacy reasons
+#         :param loop_axis: ignored for now - added for legacy reasons
+#         :param num_mp_procs: ignored for now - added for legacy reasons
+#         :param pad_to_pow2: ignored for now - added for legacy reasons
+#         :return: resampled time series
+#         '''
+#
+#         from scipy.signal import resample
+#         samplerate = self.attrs['samplerate']
+#
+#
+#         time_axis = self['time']
+#         time_axis_length = np.squeeze(time_axis.shape)
+#         new_length = int(np.round(time_axis_length*resampled_rate/float(samplerate)))
+#
+#         # print new_length
+#
+#         # if self.time_axis_index<0:
+#         #     self.time_axis_index = get_axis_index(data_array=self, axis_name='time')
+#
+#         # time_axis = self.coords[ self.dims[self.time_axis_index] ]
+#
+#         # time_axis = self['time']
+#
+#         resampled_array, new_time_axis = resample(self.values,
+#                                          new_length, t=time_axis.values,
+#                                          axis=self.time_axis_index, window=window)
+#
+#
+#         # print new_time_axis
+#
+#         #constructing axes
+#         coords = []
+#         for i, dim_name in enumerate(self.dims):
+#             if i != self.time_axis_index:
+#                 coords.append(self.coords[dim_name].copy())
+#             else:
+#                 coords.append((dim_name,new_time_axis))
+#
+#
+#         resampled_time_series = xray.DataArray(resampled_array, coords=coords)
+#         resampled_time_series.attrs['samplerate'] = resampled_rate
+#
+#         return resampled_time_series
+#
+#
+#     def remove_buffer(self, duration):
+# 	"""
+#         Remove the desired buffer duration (in seconds) and reset the
+# 	time range.
+#
+#         Parameter
+#         ---------
+#         duration : {int,float,({int,float},{int,float})}
+#             The duration to be removed. The units depend on the samplerate:
+#             E.g., if samplerate is specified in Hz (i.e., samples per second),
+#             the duration needs to be specified in seconds and if samplerate is
+#             specified in kHz (i.e., samples per millisecond), the duration needs
+#             to be specified in milliseconds.
+#             A single number causes the specified duration to be removed from the
+#             beginning and end. A 2-tuple can be passed in to specify different
+#             durations to be removed from the beginning and the end respectively.
+#
+#         Returns
+#         -------
+#         ts : {TimeSeries}
+#             A TimeSeries instance with the requested durations removed from the
+#             beginning and/or end.
+#         """
+#
+#         number_of_buffer_samples =  int(np.ceil(duration*self.attrs['samplerate']))
+#         if number_of_buffer_samples > 0:
+#             return  self[:,:,number_of_buffer_samples:-number_of_buffer_samples]
+#
+#
+#
+#     def baseline_corrected(self, base_range):
+#         """
+#
+#         Return a baseline corrected timeseries by subtracting the
+#         average value in the baseline range from all other time points
+#         for each dimension.
+#
+#         Parameters
+#         ----------
+#         base_range: {tuple}
+#             Tuple specifying the start and end time range (inclusive)
+#             for the baseline.
+#
+#         Returns
+#         -------
+#         ts : {TimeSeries}
+#             A TimeSeries instance with the baseline corrected data.
+#
+#         """
+#
+#         return self - self.isel(time=(self['time'] >= base_range[0]) & (self['time'] <= base_range[1])).mean(dim='time')
