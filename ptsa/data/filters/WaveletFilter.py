a6dad9106d689a6cb05f0f7c732e6c60a07e565b
Maciek Swat <maciekswat@gmail.com>
2016-01-15 11:36:42 -0500

    Fixed waveletFilter to work with session dat and with chopped events

--- ptsa/data/filters/WaveletFilter.py
+++ ptsa/data/filters/WaveletFilter.py
@@ -75,16 +75,29 @@ class WaveletFilter(PropertiedObject):
         # )
 
 
+        # depending on the reader channel axis may be a rec array or a simple array
+        # we are interested in an array that has channel labels
+        time_series_channel_axis = self.time_series['channels'].data
+        try:
+            time_series_channel_axis = time_series_channel_axis['name']
+        except (KeyError,IndexError):
+            pass
+
         samplerate = self.time_series.attrs['samplerate']
 
         for e, ev in enumerate(self.time_series['events']):
             for b, bp_pair in enumerate(self.bipolar_pairs):
 
-                pow_array[b,e,:,:] = -1.0
                 print 'bp_pair=',bp_pair, ' event num = ',e
 
-                ch0 = self.time_series.isel(channels=(self.time_series['channels']==bp_pair['ch0'])).values
-                ch1 = self.time_series.isel(channels=(self.time_series['channels']==bp_pair['ch1'])).values
+                ch0 = self.time_series.isel(channels=(time_series_channel_axis == bp_pair['ch0']), events=e).values
+                ch1 = self.time_series.isel(channels=(time_series_channel_axis == bp_pair['ch1']), events=e).values
+
+                # ch0 = self.time_series.isel(channels=(time_series_channel_axis == bp_pair['ch0'])).values
+                # ch1 = self.time_series.isel(channels=(time_series_channel_axis == bp_pair['ch1'])).values
+
+                # ch0 = self.time_series.isel(channels=(self.time_series['channels']==bp_pair['ch0'])).values
+                # ch1 = self.time_series.isel(channels=(self.time_series['channels']==bp_pair['ch1'])).values
 
 
                 bp_data = ch0-ch1
@@ -97,7 +110,7 @@ class WaveletFilter(PropertiedObject):
 
                 if self.resamplerate>0.0:
                     bp_data_wavelet = resample(bp_data_wavelet, num=rs_time_axis.shape[0], axis=1)
-                print bp_data_wavelet
+                # print bp_data_wavelet
                 #
 
 
@@ -122,9 +135,7 @@ class WaveletFilter(PropertiedObject):
         return pow_array
 
 
-
-
-if __name__=='__main__':
+def test_1():
 
     import time
     start = time.time()
@@ -168,9 +179,10 @@ if __name__=='__main__':
 
 
     wf = WaveletFilter(time_series=first_session_data,
-                       bipolar_pairs=bipolar_pairs,
+                       bipolar_pairs=bipolar_pairs[0:3],
                        freqs=np.logspace(np.log10(3), np.log10(180), 12),
-                       resamplerate=50.0)
+                       # resamplerate=50.0
+                       )
 
     pow_wavelet = wf.filter()
 
@@ -183,3 +195,72 @@ if __name__=='__main__':
 
 
     print 'total time = ',time.time()-start
+
+
+    return chopped_wavelets
+
+def test_2():
+
+
+    import time
+    start = time.time()
+
+    e_path = '/Users/m/data/events/RAM_FR1/R1060M_events.mat'
+
+    from ptsa.data.readers import BaseEventReader
+
+    base_e_reader = BaseEventReader(event_file=e_path, eliminate_events_with_no_eeg=True, use_ptsa_events_class=False)
+
+    base_e_reader.read()
+
+    base_events = base_e_reader.get_output()
+
+    base_events = base_events[base_events.type == 'WORD']
+
+    # selecting only one session
+    base_events = base_events[base_events.eegfile == base_events[0].eegfile]
+
+
+
+    from ptsa.data.readers.TalReader import TalReader
+    tal_path = '/Users/m/data/eeg/R1060M/tal/R1060M_talLocs_database_bipol.mat'
+    tal_reader = TalReader(tal_filename=tal_path)
+    monopolar_channels = tal_reader.get_monopolar_channels()
+    bipolar_pairs = tal_reader.get_bipolar_pairs()
+
+    print 'bipolar_pairs=',bipolar_pairs
+
+
+
+    from ptsa.data.readers.TimeSeriesEEGReader import TimeSeriesEEGReader
+
+    time_series_reader = TimeSeriesEEGReader(events=base_events, start_time=0.0,
+                                             end_time=1.6, buffer_time=1.0, keep_buffer=True)
+
+    base_eegs = time_series_reader.read(channels=monopolar_channels)
+
+
+
+
+
+
+
+    wf = WaveletFilter(time_series=base_eegs,
+                       bipolar_pairs=bipolar_pairs[0:3],
+                       freqs=np.logspace(np.log10(3), np.log10(180), 12),
+                       # resamplerate=50.0
+                       )
+
+    pow_wavelet = wf.filter()
+
+
+
+
+    print 'total time = ',time.time()-start
+
+    return pow_wavelet
+
+if __name__=='__main__':
+    edcw = test_1()
+    pow_wavelet = test_2()
+    print
