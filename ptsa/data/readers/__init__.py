bae65acfcbc6d5a28c6d51883bb124f3cf5cf86d
Maciek Swat <maciekswat@gmail.com>
2016-01-23 14:42:36 -0500

    Added ParamsReader

--- ptsa/data/readers/__init__.py
+++ ptsa/data/readers/__init__.py
@@ -2,3 +2,4 @@ from PTSAEventReader import PTSAEventReader
 from BaseEventReader import BaseEventReader
 from TimeSeriesEEGReader import TimeSeriesEEGReader
 from NetCDF4XrayReader import NetCDF4XrayReader
+from ParamsReader import ParamsReader
