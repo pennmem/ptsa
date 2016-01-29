a545193c363b4a002b77665994676e35d7b02d4e
Maciek Swat <maciekswat@gmail.com>
2016-01-29 11:32:03 -0500

    Code cleanup

--- ptsa/data/readers/BaseEventReader.py
+++ ptsa/data/readers/BaseEventReader.py
@@ -3,11 +3,6 @@ __author__ = 'm'
 from os.path import *
 import numpy as np
 import re
-# from scipy.io import loadmat
-
-import sys
-
-sys.path.append('/Users/m/PTSA_NEW_GIT')
 
 from ptsa.data.events import Events
 
@@ -16,35 +11,50 @@ from ptsa.data.common.path_utils import find_dir_prefix
 from ptsa.data.common import pathlib
 
 class BaseEventReader(PropertiedObject):
+    '''
+    Reader class that reads event file and returns them as np.recarray
+    '''
     _descriptors = [
-        TypeValTuple('samplerate', float, 20),
-        TypeValTuple('order', str, ''),
+        TypeValTuple('event_file', str, ''),
         TypeValTuple('eliminate_events_with_no_eeg', bool, True),
-        TypeValTuple('raw_data_root', str, None),
         TypeValTuple('use_ptsa_events_class', bool, False),
         TypeValTuple('use_reref_eeg', bool, False),
     ]
+    def __init__(self,**kwds):
+        '''
+        Constructor:
 
-    def __init__(self, event_file, **kwds):
-        self._event_file = event_file
-        self._events = None
+        :param kwds:allowed values are:
+        -------------------------------------
+        :param event_file {str} -  path to event file
+        :param eliminate_events_with_no_eeg {bool} - flag to automatically remov events woth no eegfile (default True)
+        :param use_reref_eeg {bool} -  flag that changes eegfiles to point reref eegs. Default is False and eegs read
+        are nonreref ones
+        :return: None
+        '''
+        self.init_attrs(kwds)
 
-        possible_argument_list = ['eliminate_events_with_no_eeg', 'use_ptsa_events_class',
-                                  'use_reref_eeg']
 
-        for argument in possible_argument_list:
-            try:
-                accessor = getattr(self, argument)
-                accessor = argument
-            except AttributeError:
-                sys.exit()
-                pass
-            # try:
-            #     setattr(self, argument, kwds[argument])
-            # except LookupError:
-            #     print 'did not find the argument: ', argument
-            #     pass
-
+    # def __init__(self, event_file, **kwds):
+    #     self._event_file = event_file
+    #     self._events = None
+    #
+    #     possible_argument_list = ['eliminate_events_with_no_eeg', 'use_ptsa_events_class',
+    #                               'use_reref_eeg']
+    #
+    #     for argument in possible_argument_list:
+    #         try:
+    #             accessor = getattr(self, argument)
+    #             accessor = argument
+    #         except AttributeError:
+    #             sys.exit()
+    #             pass
+    #         # try:
+    #         #     setattr(self, argument, kwds[argument])
+    #         # except LookupError:
+    #         #     print 'did not find the argument: ', argument
+    #         #     pass
+    #
     def correct_eegfile_field(self, events):
         data_dir_bad = r'/data.*/' + events[0].subject + r'/eeg'
         data_dir_good = r'/data/eeg/' + events[0].subject + r'/eeg'
@@ -88,58 +98,23 @@ class BaseEventReader(PropertiedObject):
         if not self.use_reref_eeg:
             evs = self.correct_eegfile_field(evs)
 
-        self._events = evs
-
-        return self._events
-
-
-    # def read(self):
-    #     from ptsa.data.MatlabIO import read_single_matlab_matrix_as_numpy_structured_array
-    #
-    #     # extract matlab matrix (called 'events') as numpy structured array
-    #     struct_array = read_single_matlab_matrix_as_numpy_structured_array(self._event_file, 'events')
-    #
-    #     evs = struct_array
-    #
-    #     if self.eliminate_events_with_no_eeg:
-    #
-    #         # eliminating events that have no eeg file
-    #         indicator = np.empty(len(evs), dtype=bool)
-    #         indicator[:] = False
-    #
-    #         for i, ev in enumerate(evs):
-    #             # MAKE THIS CHECK STRONGER
-    #             indicator[i] = (type(evs[i].eegfile).__name__.startswith('unicode')) & (len(str(evs[i].eegfile)) > 3)
-    #
-    #         evs = evs[indicator]
-    #
-    #     # determining data_dir_prefix in case rhino /data filesystem was mounted under different root
-    #     data_dir_prefix = self.find_data_dir_prefix()
-    #     for i, ev in enumerate(evs):
-    #         ev.eegfile=join(data_dir_prefix, str(pathlib.Path(str(ev.eegfile)).parts[1:]))
-    #
-    #
-    #     # NEW CODE
-    #     if self.use_ptsa_events_class:
-    #         evs = Events(evs)
-    #
-    #     if not self.use_reref_eeg:
-    #         evs = self.correct_eegfile_field(evs)
-    #
-    #     self._events = evs
-    #
-    #     return self._events
+        return evs
 
 
     def find_data_dir_prefix(self):
-        # determining dir_prefix
-        #
-        # data on rhino is mounted as /data
-        # copying rhino /data structure to another directory will cause all files in data have new prefix
-        # example:
-        # self._event_file='/Users/m/data/events/R1060M_events.mat'
-        # prefix is '/Users/m'
-        # we use find_dir_prefix to determine prefix based on common_root in path with and without prefix
+        '''
+        determining dir_prefix
+
+        data on rhino database is mounted as /data
+        copying rhino /data structure to another directory will cause all files in data have new prefix
+        example:
+        self._event_file='/Users/m/data/events/R1060M_events.mat'
+        prefix is '/Users/m'
+        we use find_dir_prefix to determine prefix based on common_root in path with and without prefix
+
+        :return: data directory prefix
+        '''
+
         common_root = 'data/events'
         prefix = find_dir_prefix(path_with_prefix=self._event_file, common_root=common_root)
         if not prefix:
@@ -148,20 +123,14 @@ class BaseEventReader(PropertiedObject):
 
         return find_dir_prefix(self._event_file, 'data/events')
 
-    # def get_raw_data_root(self):
-    #     return self.raw_data_root
-
-    def get_output(self):
-        return self._events
-
-    def set_output(self, evs):
-        self._events = evs
-
 
 if __name__ == '__main__':
     from BaseEventReader import BaseEventReader
     # e_path = join('/Volumes/rhino_root', 'data/events/RAM_FR1/R1060M_math.mat')
-    e_path = '/Users/m/data/events/RAM_FR1/R1056M_events.mat'
+    e_path = '/Users/m/data/events/RAM_PS/R1108J_1_events.mat'
+
+    # e_path ='/Users/m/data/events/RAM_FR1/R1056M_events.mat'
+
     e_reader = BaseEventReader(event_file=e_path, eliminate_events_with_no_eeg=True)
 
 
