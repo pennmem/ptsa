e6e86895b263b9fe280212c0529c909b143cf222
Maciek Swat <maciekswat@gmail.com>
2016-02-03 18:47:35 -0500

    Added test to check filters

---
+++ ptsa/tests/test_filters.py
@@ -0,0 +1,105 @@
+__author__ = 'm'
+
+import numpy as np
+from numpy.testing import *
+import unittest
+from ptsa.data.readers import BaseEventReader
+from ptsa.data.readers.EEGReader import EEGReader
+from ptsa.data.filters.ButterworthFilter import ButterworthFiler
+from ptsa.data.filters.ResampleFilter import ResampleFilter
+from ptsa.data.filters.MorletWaveletFilter import MorletWaveletFilter
+from ptsa.data.readers.TalReader import TalReader
+from ptsa.data.readers import EEGReader
+from ptsa.data.filters import EventDataChopper
+
+class test_filters(unittest.TestCase):
+
+    def setUp(self):
+        self.event_range = range(0, 30, 1)
+        self.e_path = '/Users/m/data/events/RAM_FR1/R1060M_events.mat'
+
+        tal_path = '/Users/m/data/eeg/R1060M/tal/R1060M_talLocs_database_bipol.mat'
+        tal_reader = TalReader(filename=tal_path)
+        self.monopolar_channels = tal_reader.get_monopolar_channels()
+
+        # ---------------- NEW STYLE PTSA -------------------
+        base_e_reader = BaseEventReader(filename=self.e_path, eliminate_events_with_no_eeg=True)
+
+        base_events = base_e_reader.read()
+
+        base_events = base_events[base_events.type == 'WORD']
+
+        base_ev_order = np.argsort(base_events, order=('session', 'list', 'mstime'))
+        self.base_events = base_events[base_ev_order]
+
+        # retaining first session
+        dataroot = self.base_events[0].eegfile
+        self.base_events = self.base_events[self.base_events.eegfile == dataroot]
+
+        self.base_events = self.base_events[self.event_range]
+
+        # self.base_events = base_events[self.event_range]
+
+        # self.base_events = self.read_base_events()
+
+        eeg_reader = EEGReader(events=self.base_events, channels=self.monopolar_channels,
+                               start_time=0.0, end_time=1.6, buffer_time=1.0)
+
+        self.base_eegs = eeg_reader.read()
+
+        session_reader = EEGReader(session_dataroot=dataroot, channels=self.monopolar_channels)
+        self.session_eegs = session_reader.read()
+
+    def test_event_data_chopper(self):
+
+        dataroot = self.base_events[0].eegfile
+
+        session_reader = EEGReader(session_dataroot=dataroot, channels=self.monopolar_channels)
+        session_eegs = session_reader.read()
+
+
+        sedc = EventDataChopper(events=self.base_events, session_data=session_eegs, start_time=0.0, end_time=1.6,
+                                buffer_time=1.0)
+        chopped_session = sedc.filter()
+
+        assert_array_equal(chopped_session, self.base_eegs)
+
+    def test_wavelets_with_event_data_chopper(self):
+        wf_session = MorletWaveletFilter(
+            time_series=self.session_eegs[:, :, :self.session_eegs.shape[2] / 4],
+            freqs=np.logspace(np.log10(3), np.log10(180), 8),
+            output='power',
+            frequency_dim_pos=0,
+            verbose=True
+        )
+
+        pow_wavelet_session, phase_wavelet_session = wf_session.filter()
+
+
+        sedc = EventDataChopper(events=self.base_events, session_data=pow_wavelet_session, start_time=0.0, end_time=1.6,
+                                buffer_time=1.0)
+        chopped_session_pow_wavelet = sedc.filter()
+
+        # removing buffer
+        chopped_session_pow_wavelet = chopped_session_pow_wavelet[:, :, :, 500:-500]
+
+        wf = MorletWaveletFilter(time_series=self.base_eegs,
+                                 freqs=np.logspace(np.log10(3), np.log10(180), 8),
+                                 output='power',
+                                 frequency_dim_pos=0,
+                                 verbose=True
+                                 )
+
+        pow_wavelet, phase_wavelet = wf.filter()
+
+        pow_wavelet = pow_wavelet[:, :, :, 500:-500]
+
+        assert_array_almost_equal(
+                                (chopped_session_pow_wavelet.data - pow_wavelet.data) / pow_wavelet.data,
+                                np.zeros_like(pow_wavelet),
+                                decimal=5
+                                )
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
