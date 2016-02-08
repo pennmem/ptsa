bdf8d04de779f76b40085c9ea34ed0bcfe4baa12
Maciek Swat <maciekswat@gmail.com>
2016-02-08 17:59:54 -0500

    Changed handing of the samplerate - now it is attached to time series as axis

--- ptsa/data/filters/ButterworthFilter.py
+++ ptsa/data/filters/ButterworthFilter.py
@@ -39,7 +39,7 @@ class ButterworthFiler(PropertiedObject):
 
         time_axis_index = get_axis_index(self.time_series, axis_name='time')
         filtered_array = buttfilt(self.time_series,
-                                  self.freq_range, self.time_series['samplerate'].data, self.filt_type,
+                                  self.freq_range, float(self.time_series['samplerate']), self.filt_type,
                                   self.order, axis=time_axis_index)
 
         coords_dict = {coord_name: DataArray(coord.copy()) for coord_name, coord in self.time_series.coords.items()}
@@ -52,7 +52,52 @@ class ButterworthFiler(PropertiedObject):
         )
 
         # filtered_time_series.attrs['samplerate'] = self.time_series.attrs['samplerate']
-        filtered_time_series.attrs['samplerate'] = self.time_series['samplerate']
+        # filtered_time_series.attrs['samplerate'] = self.time_series['samplerate']
         filtered_time_series = TimeSeriesX(filtered_time_series)
 
         return filtered_time_series
+
+if __name__ == '__main__':
+    import numpy as np
+    from numpy.testing import *
+    from ptsa.data.readers import BaseEventReader
+    from ptsa.data.filters.MorletWaveletFilter import MorletWaveletFilter
+    from ptsa.data.filters.ButterworthFilter import ButterworthFiler
+    from ptsa.data.readers.TalReader import TalReader
+    from ptsa.data.readers import EEGReader
+    from ptsa.data.readers import PTSAEventReader
+    from ptsa.data.events import Events
+
+    e_path = '/Volumes/rhino_root/data/events/RAM_PS/R1108J_1_events.mat'
+    e_path = '/Volumes/rhino_root/data/events/RAM_PS/R1108J_1_events.mat'
+    # e_path ='/Users/m/data/events/RAM_FR1/R1056M_events.mat'
+
+    e_reader = BaseEventReader(filename=e_path, eliminate_events_with_no_eeg=True)
+
+
+    events = e_reader.read()
+
+    from ptsa.data.readers.TalReader import TalReader
+
+    tal_path = '/Volumes/rhino_root/data/eeg/R1108J_1/tal/R1108J_1_talLocs_database_bipol.mat'
+    tal_reader = TalReader(filename=tal_path)
+    monopolar_channels = tal_reader.get_monopolar_channels()
+    bipolar_pairs = tal_reader.get_bipolar_pairs()
+
+    # ---------------- NEW STYLE PTSA -------------------
+    base_e_reader = BaseEventReader(filename=e_path, eliminate_events_with_no_eeg=True)
+
+    base_events = base_e_reader.read()
+
+    base_events = base_events[(base_events.type == 'STIMULATING') ]
+    base_events = base_events[base_events.session == 2]
+
+
+    from ptsa.data.readers.EEGReader import EEGReader
+    eeg_reader = EEGReader(events=base_events, channels=monopolar_channels[0:3],
+                           start_time=-1.1, end_time=-0.1, buffer_time=1.0)
+
+    base_eegs = eeg_reader.read()
+
+    bw_base_eegs = base_eegs.filtered(freq_range=[58.,62.], filt_type='stop', order=4)
+
