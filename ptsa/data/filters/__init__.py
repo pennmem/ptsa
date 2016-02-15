dbbe974070f8bb7ad89bd758e28e0831a0db9dec
Maciek Swat <maciekswat@gmail.com>
2016-02-15 13:46:55 -0500

    Added cpp version of MorletWaveletFilter

--- ptsa/data/filters/__init__.py
+++ ptsa/data/filters/__init__.py
@@ -3,3 +3,10 @@ from MonopolarToBipolarMapper import MonopolarToBipolarMapper
 from MorletWaveletFilter import MorletWaveletFilter
 from ResampleFilter import ResampleFilter
 from EventDataChopper import EventDataChopper
+
+try:
+    from MorletWaveletFilterCpp import MorletWaveletFilterCpp
+except ImportError as ie:
+    print 'Could not import MorletWaveletFilterCpp (C++ version of MorletWaveletFilter): '+ str(ie)
+    print 'You can still use MorletWaveletFilter'
+
