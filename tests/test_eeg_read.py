d7e8d1cc0e75063e9efb2ed493c7730f9c866fbd
Michael V. DePalatis <mike@depalatis.net>
2017-05-04 11:04:52 -0400

    Cleanup

--- tests/test_eeg_read.py
+++ tests/test_eeg_read.py
@@ -1,6 +1,11 @@
 import unittest
-from ptsa.data.events import Events
+from numpy.testing import assert_array_equal
 import pytest
+
+from ptsa.data.filters.ButterworthFilter import ButterworthFilter
+from ptsa.data.filters.ResampleFilter import ResampleFilter
+from ptsa.data.events import Events
+from ptsa.data.experimental.TimeSeriesEEGReader import TimeSeriesEEGReader
 
 from EventReadersTestBase import EventReadersTestBase
 
@@ -16,7 +21,8 @@ class TestEEGRead(unittest.TestCase, EventReadersTestBase):
 
         events = self.read_ptsa_events()
 
-        # in case fancy indexing looses Eventness of events we need to create Events object explicitely
+        # in case fancy indexing looses Eventness of events we need to create
+        # Events object explicitely
         if not isinstance(events, Events):
             events = Events(events)
 
@@ -25,8 +31,6 @@ class TestEEGRead(unittest.TestCase, EventReadersTestBase):
         eegs = events.get_data(channels=['002', '003'], start_time=0.0, end_time=1.6,
                                buffer_time=1.0, eoffset='eegoffset', keep_buffer=False,
                                eoffset_in_time=False, verbose=True)
-
-        from ptsa.data.experimental.TimeSeriesEEGReader import TimeSeriesEEGReader
 
         time_series_reader = TimeSeriesEEGReader(base_events)
 
@@ -42,11 +46,11 @@ class TestEEGRead(unittest.TestCase, EventReadersTestBase):
         assert_array_equal(eegs, base_eegs.data)
 
     def test_eeg_read_keep_buffer(self):
-
         # OLD READERS
         events = self.read_ptsa_events()
 
-        # in case fancy indexing looses Eventness of events we need to create Events object explicitely
+        # in case fancy indexing looses Eventness of events we need to create
+        # Events object explicitely
         if not isinstance(events, Events):
             events = Events(events)
 
@@ -54,69 +58,50 @@ class TestEEGRead(unittest.TestCase, EventReadersTestBase):
                                buffer_time=1.0, eoffset='eegoffset', keep_buffer=True,
                                eoffset_in_time=False, verbose=True)
 
-
         # NEW READERS
         base_events = self.read_base_events()
-
-        from ptsa.data.experimental.TimeSeriesEEGReader import TimeSeriesEEGReader
-
         time_series_reader = TimeSeriesEEGReader(events=base_events, start_time=0.0,
                                                  end_time=1.6, buffer_time=1.0, keep_buffer=True)
 
         time_series_reader.read(channels=['002', '003'])
-
         base_eegs = time_series_reader.get_output()
 
         # testing
         assert_array_equal(eegs, base_eegs.data)
 
     def test_eeg_read_keep_buffer_butterworth_filtered(self):
-
         # old readers
         events = self.read_ptsa_events()
 
-        # in case fancy indexing looses Eventness of events we need to create Events object explicitely
+        # in case fancy indexing looses Eventness of events we need to create
+        # Events object explicitely
         if not isinstance(events, Events):
             events = Events(events)
 
         base_events = self.read_base_events()
-
         eegs = events.get_data(channels=['002', '003'], start_time=0.0, end_time=1.6,
                                buffer_time=1.0, eoffset='eegoffset', keep_buffer=True,
                                eoffset_in_time=False, verbose=True)
         # filtering
         eegs_filtered = eegs.filtered([58, 62], filt_type='stop', order=4)
 
-
         # New style reading
-        from ptsa.data.experimental.TimeSeriesEEGReader import TimeSeriesEEGReader
-
         time_series_reader = TimeSeriesEEGReader(events=base_events, start_time=0.0,
                                                  end_time=1.6, buffer_time=1.0, keep_buffer=True)
-
         time_series_reader.read(channels=['002', '003'])
-
         base_eegs = time_series_reader.get_output()
 
-        from ptsa.data.filters.ButterworthFilter import ButterworthFilter
         b_filter = ButterworthFilter(time_series=base_eegs, samplerate=base_eegs.attrs['samplerate'],
                                      freq_range=[58, 62], filt_type='stop',
                                      order=4)
 
-
         base_eegs_filtered = b_filter.filter()
-
-
 
         # testing
         assert_array_equal(eegs_filtered, base_eegs_filtered.data)
-
         assert_array_equal(eegs, base_eegs.data)
 
     def test_eeg_resample(self):
-
-
-
         # old readers
         events = self.read_ptsa_events()
 
@@ -132,29 +117,16 @@ class TestEEGRead(unittest.TestCase, EventReadersTestBase):
         # filtering
         eegs_resampled = eegs.resampled(50.0)
 
-
         # New style reading
-        from ptsa.data.experimental.TimeSeriesEEGReader import TimeSeriesEEGReader
-
         time_series_reader = TimeSeriesEEGReader(events=base_events, start_time=0.0,
                                                  end_time=1.6, buffer_time=1.0, keep_buffer=True)
 
         time_series_reader.read(channels=['002', '003'])
 
         base_eegs = time_series_reader.get_output()
-
-        from ptsa.data.filters.ResampleFilter import ResampleFilter
         resample_filter = ResampleFilter(time_series=base_eegs, resamplerate=50.0)
 
         base_eegs_resampled = resample_filter.filter()
 
-
         # testing
         assert_array_equal(eegs_resampled, base_eegs_resampled.data)
-
-
-
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
