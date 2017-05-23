0400cabd947d7f9ad065d3903ed9d3ab5a14fdf9
Michael V. DePalatis <mike@depalatis.net>
2017-05-23 12:21:02 -0400

    Wrap non-test in a function
    To be fixed to actually test something

--- tests/test_wavelet_cpp.py
+++ tests/test_wavelet_cpp.py
@@ -2,34 +2,36 @@ import sys
 from ptsa.extensions.morlet import morlet
 from scipy.stats import describe
 import numpy as np
-num_freqs = 8
-min_frequency = 3.0
-max_frequency = 60.0
 
-morlet_transform = morlet.MorletWaveletTransform(5, np.logspace(np.log10(min_frequency), np.log10(max_frequency), num_freqs),  1000, 4096)
-# morlet_transform = morlet.MorletWaveletTransform(5, min_frequency, max_frequency, num_freqs, 1000, 4096)
 
-# morlet_transform = morlet.MorletWaveletTransform()
-# morlet_transform.init_flex(5, np.logspace(np.log10(min_frequency), np.log10(max_frequency), num_freqs),  1000, 4096)
-# morlet_transform.init(5, min_frequency, max_frequency, num_freqs, 1000, 4096)
+def fixme():
+    num_freqs = 8
+    min_frequency = 3.0
+    max_frequency = 60.0
 
-samplerate = 1000.
-frequency = 60.0
-modulation_frequency = 80.0
+    morlet_transform = morlet.MorletWaveletTransform(5, np.logspace(np.log10(min_frequency), np.log10(max_frequency), num_freqs),  1000, 4096)
+    # morlet_transform = morlet.MorletWaveletTransform(5, min_frequency, max_frequency, num_freqs, 1000, 4096)
 
-duration = 4.096
+    # morlet_transform = morlet.MorletWaveletTransform()
+    # morlet_transform.init_flex(5, np.logspace(np.log10(min_frequency), np.log10(max_frequency), num_freqs),  1000, 4096)
+    # morlet_transform.init(5, min_frequency, max_frequency, num_freqs, 1000, 4096)
 
-n_points = int(np.round(duration*samplerate))
-x = np.arange(n_points, dtype=np.float)
-signal = np.sin(x*(2*np.pi*frequency/samplerate))-np.cos(x*(2*np.pi*frequency/samplerate))
+    samplerate = 1000.
+    frequency = 60.0
+    modulation_frequency = 80.0
 
-powers=np.empty(shape=(signal.shape[0]*num_freqs,), dtype=np.float)
-num_of_iterations = 100
-# for i in xrange(num_of_iterations):
-#     morlet_transform.multiphasevec(signal,powers)
-morlet_transform.multiphasevec(signal,powers)
+    duration = 4.096
 
-powers = powers.reshape(8,powers.shape[0]/8)
+    n_points = int(np.round(duration*samplerate))
+    x = np.arange(n_points, dtype=np.float)
+    signal = np.sin(x*(2*np.pi*frequency/samplerate))-np.cos(x*(2*np.pi*frequency/samplerate))
 
-print(describe(powers))
+    powers=np.empty(shape=(signal.shape[0]*num_freqs,), dtype=np.float)
+    num_of_iterations = 100
+    # for i in xrange(num_of_iterations):
+    #     morlet_transform.multiphasevec(signal,powers)
+    morlet_transform.multiphasevec(signal,powers)
 
+    powers = powers.reshape(8,powers.shape[0]/8)
+
+    print(describe(powers))
