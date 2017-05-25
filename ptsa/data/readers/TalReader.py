14f184ec2579283e571d4d96f26232be16bf5c52
leond <lad258@cornell.edu>
2017-05-25 14:46:25 -0400

    TalReader now should read bipolar pairs and monopolar channels from pairs.json

--- ptsa/data/readers/TalReader.py
+++ ptsa/data/readers/TalReader.py
@@ -3,11 +3,14 @@ __author__ = 'm'
 import numpy as np
 from ptsa.data.common import TypeValTuple, PropertiedObject
 from ptsa.data.readers import BaseReader
+import os
+import json
+import pandas as pd
 
 
 class TalReader(PropertiedObject,BaseReader):
     '''
-    Reader that reads tal structs Matlab file and converts it to numpy recarray
+    Reader that reads tal structs Matlab or JSON file and converts it to numpy recarray
     '''
     _descriptors = [
         TypeValTuple('filename', str, ''),
@@ -20,7 +23,7 @@ class TalReader(PropertiedObject,BaseReader):
 
         :param kwds:allowed values are:
         -------------------------------------
-        :param filename {str} -  path to tal file
+        :param filename {str} -  path to tal file or pairs.json file
         :param struct_name {str} -  name of the matlab struct to load
         :return: None
         '''
@@ -29,6 +32,8 @@ class TalReader(PropertiedObject,BaseReader):
         self.bipolar_channels=None
 
         self.tal_structs_array = None
+        self._json = os.path.splitext(self.filename)[-1]=='json'
+
 
     def get_bipolar_pairs(self):
         '''
@@ -60,30 +65,45 @@ class TalReader(PropertiedObject,BaseReader):
             self.bipolar_channels[i] = tuple(map(lambda x: str(x).zfill(3), channel_array))
 
 
+    @staticmethod
+    def from_dict(pairs):
+        pairs = pd.DataFrame.from_dict(pairs.values()[0]['pairs'], orient='index').sort_values(by=['channel_1','channel_2'])
+        pairs.index.name = 'tagName'
+        pairs['channel'] = [[ch1, ch2] for ch1, ch2 in zip(pairs.channel_1.values, pairs.channel_2.values)]
+        pairs['eType'] = pairs.type_1
+        return pairs.to_records()
+
     def read(self):
         '''
 
         :return:np.recarray representing tal struct array (originally defined in Matlab file)
         '''
+        if not self._json:
+            from ptsa.data.MatlabIO import read_single_matlab_matrix_as_numpy_structured_array
 
-        from ptsa.data.MatlabIO import read_single_matlab_matrix_as_numpy_structured_array
+            struct_names = ['bpTalStruct','subjTalEvents']
+            # struct_names = ['bpTalStruct']
+            if self.struct_name not in struct_names:
+                self.tal_struct_array = read_single_matlab_matrix_as_numpy_structured_array(self.filename, self.struct_name,verbose=False)
 
-        struct_names = ['bpTalStruct','subjTalEvents']
-        # struct_names = ['bpTalStruct']
-        if self.struct_name not in struct_names:
-            self.tal_struct_array = read_single_matlab_matrix_as_numpy_structured_array(self.filename, self.struct_name,verbose=False)
-
-            if self.tal_struct_array is not None:
-                return self.tal_struct_array
-            else:
-                raise AttributeError('Could not read tal struct data for the specified struct_name='+self.struct_name)
-
-        else:
-
-            for sn in struct_names:
-                self.tal_struct_array = read_single_matlab_matrix_as_numpy_structured_array(self.filename,sn,verbose=False)
                 if self.tal_struct_array is not None:
                     return self.tal_struct_array
+                else:
+                    raise AttributeError('Could not read tal struct data for the specified struct_name='+self.struct_name)
+
+            else:
+
+                for sn in struct_names:
+                    self.tal_struct_array = read_single_matlab_matrix_as_numpy_structured_array(self.filename,sn,verbose=False)
+                    if self.tal_struct_array is not None:
+                        return self.tal_struct_array
+        else:
+            with open(self.filename) as fp:
+                pairs= json.load(fp)
+            self.tal_struct_array = self.from_dict(pairs)
+            return self.tal_struct_array
+
+
 
         raise AttributeError('Could not read tal struct data. Try specifying struct_name argument :'
                              '\nTalReader(filename=e_path, struct_name=<name_of_struc_to_read>)')
