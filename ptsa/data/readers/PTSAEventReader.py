c8b897abf1ac71b8dac9d40ade7275dd1762006f
Michael V. DePalatis <mike@depalatis.net>
2017-05-31 09:44:03 -0400

    PEP8ify docstrings

--- ptsa/data/readers/PTSAEventReader.py
+++ ptsa/data/readers/PTSAEventReader.py
@@ -1,5 +1,3 @@
-__author__ = 'm'
-
 import numpy as np
 from ptsa.data.events import Events
 from ptsa.data.rawbinwrapper import RawBinWrapper
@@ -7,11 +5,12 @@ from .BaseEventReader import BaseEventReader
 from ptsa.data.common import TypeValTuple
 from ptsa.data.readers import BaseReader
 
-class PTSAEventReader(BaseEventReader,BaseReader):
-    '''
+
+class PTSAEventReader(BaseEventReader, BaseReader):
+    """
     Event reader that returns original PTSA Events object with attached rawbinwrappers
     rawbinwrappers are objects that know how to read eeg binary data
-    '''
+    """
 
     _descriptors = [
 
@@ -21,11 +20,9 @@ class PTSAEventReader(BaseEventReader,BaseReader):
     ]
 
     def __init__(self, **kwds):
-        '''
-        Constructor:
-
-        :param kwds:allowed values are:
-        -------------------------------------
+        """
+        Keyword arguments
+        -----------------
         :param filename {str} -  path to event file
         :param eliminate_events_with_no_eeg {bool} - flag to automatically remov events woth no eegfile (default True)
         :param use_reref_eeg {bool} -  flag that changes eegfiles to point reref eegs. Default is False and eegs read
@@ -35,14 +32,15 @@ class PTSAEventReader(BaseEventReader,BaseReader):
         :param use_groupped_rawbinwrapper {bool} - flag signaling whether to use groupped rawbinwrappers
         (i.e. shared between many events with same eeglile) or not. Default is True. When True data reads are much faster
         :return: None
-        '''
+
+        """
         BaseEventReader.__init__(self, **kwds)
 
     def read(self):
-        '''
+        """
         Reads Matlab event file , converts it to np.recarray and attaches rawbinwrappers (if appropriate flags indicate so)
         :return: Events object. depending on flagg settings the rawbinwrappers may be attached as well
-        '''
+        """
 
         # calling base class read fcn
         evs = BaseEventReader.read(self)
@@ -62,12 +60,12 @@ class PTSAEventReader(BaseEventReader,BaseReader):
         return evs
 
     def attach_rawbinwrapper_groupped(self, evs):
-        '''
+        """
         attaches raw bin wrappers to individual records. Single rawbinwrapper is shared between events that have same
         eegfile
         :param evs: Events object
         :return: Events object with attached rawbinarappers
-        '''
+        """
 
         eegfiles = np.unique(evs.eegfile)
 
@@ -79,11 +77,11 @@ class PTSAEventReader(BaseEventReader,BaseReader):
                 evs[i]['esrc'] = raw_bin_wrapper
 
     def attach_rawbinwrapper_individual(self, evs):
-        '''
+        """
         attaches raw bin wrappers to individual records. Uses separate rawbinwrapper for each record
         :param evs: Events object
         :return: Events object with attached rawbinarappers
-        '''
+        """
 
         for ev in evs:
             try:
