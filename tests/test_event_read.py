158dfdbb31956aa6024220f104333a4e152623b0
Michael V. DePalatis <mike@depalatis.net>
2017-05-31 09:56:13 -0400

    Mark failing tests as expected failures
    Some of these are for old PTSA and should be removed/updated to
    work with TimeSeriesX et al.

--- tests/test_event_read.py
+++ tests/test_event_read.py
@@ -1,20 +1,18 @@
-import sys
+import os.path as osp
 import unittest
-import pytest
 
 from ptsa.data.events import Events
-from ptsa.test.utils import EventReadersTestBase
+from ptsa.test.utils import EventReadersTestBase, skip_without_rhino, get_rhino_root
 
 
-@pytest.mark.skip(reason="Hardcoded data paths")
+@skip_without_rhino
 class TestEventRead(unittest.TestCase, EventReadersTestBase):
     def setUp(self):
-        self.event_range = range(0,30,1)
-        self.e_path = '/Users/m/data/events/RAM_FR1/R1060M_events.mat'
+        self.event_range = range(0, 30, 1)
+        root = get_rhino_root()
+        self.e_path = osp.join(root, 'data', 'events', 'RAM_FR1', 'R1060M_events.mat')
 
-        if sys.platform.startswith('win'):
-            self.e_path = 'D:/data/events/RAM_FR1/R1060M_events.mat'
-
+    @unittest.expectedFailure
     def test_event_read(self):
         events = self.read_ptsa_events()
         base_events = self.read_base_events()
