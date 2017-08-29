be5342e99696ce91453f3c48680949c7f80abac8
Michael V. DePalatis <mike@depalatis.net>
2017-08-29 14:57:10 -0400

    Remove old timeseries, events, dimarray refs

--- ptsa/data/__init__.py
+++ ptsa/data/__init__.py
@@ -1,6 +1,3 @@
-from dimarray import Dim, DimArray, AttrArray
-from .timeseries import TimeSeries
-
 from .basewrapper import BaseWrapper
 from .arraywrapper import ArrayWrapper
 from .events import Events
