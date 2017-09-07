bfc298b777d31d327712b34de22b19f3e00a8c89
leond <lad258@cornell.edu>
2017-09-07 10:18:33 -0400

    factored out principle component of read() into static method, to ease testing

--- ptsa/data/readers/H5RawReader.py
+++ ptsa/data/readers/H5RawReader.py
@@ -1,4 +1,3 @@
-from .BaseReader import BaseReader
 from .BaseRawReader import BaseRawReader
 import numpy as np
 import tables
@@ -7,36 +6,7 @@ from xarray import DataArray
 class H5EEGReader(BaseRawReader):
 
     def read(self):
-        eegfile = tables.open_file(self.dataroot)
-        timeseries = eegfile.root.eeg_timeseries
-        ports = eegfile.root.ports
-        channels_to_read = np.in1d(ports, self.channels.astype(int))
-        if self.read_size < 0:
-            if 'orient' in eegfile.root.attrs and eegfile.root.attrs['orient'] == 'row':
-                eventdata = timeseries[:, channels_to_read].T
-            else:
-                eventdata = timeseries[channels_to_read, :]
-            eegfile.close()
-            return eventdata[:, None, :], np.ones((len(self.channels), 1)).astype(bool)
-
-        else:
-            eventdata = np.empty((len(self.channels, len(self.start_offsets), self.read_size)),
-                                 dtype=np.float) * np.nan
-            read_ok_mask = np.ones((len(self.channels), len(self.start_offsets))).astype(bool)
-            for i, start_offset in enumerate(self.start_offsets):
-                if 'by_row' in eegfile.root.attrs and eegfile.root.attrs['by_row'] == True:
-                    data = timeseries[start_offset:start_offset + self.read_size, channels_to_read].T
-                else:
-                    data = timeseries[channels_to_read, start_offset:start_offset + self.read_size]
-                if data.shape[-1] == self.read_size:
-                    eventdata[:, i, :] = data
-                else:
-                    print(
-                        'Cannot read full chunk of data for offset ' + str(start_offset) +
-                        'End of read interval  is outside the bounds of file ' + self.dataroot)
-                    read_ok_mask[:, i] = False
-            eegfile.close()
-
+        eventdata,read_ok_mask = self.read_h5file(self.dataroot,self.channels,self.start_offsets,self.read_size)
 
         eventdata *= self.params_dict['gain']
 
@@ -57,3 +27,36 @@ class H5EEGReader(BaseRawReader):
 
 
 
+    @staticmethod
+    def read_h5file(filename,channels,start_offsets,read_size):
+        eegfile = tables.open_file(filename)
+        timeseries = eegfile.root.eeg_timeseries
+        ports = eegfile.root.ports
+        channels_to_read = np.in1d(ports, channels.astype(int))
+        if read_size < 0:
+            if 'orient' in timeseries.attrs and timeseries.attrs['orient'] == 'row':
+                eventdata = timeseries[:, channels_to_read].T
+            else:
+                eventdata = timeseries[channels_to_read, :]
+            eegfile.close()
+            return eventdata[:, None, :], np.ones((len(channels), 1)).astype(bool)
+
+        else:
+            eventdata = np.empty((len(channels, len(start_offsets), read_size)),
+                                 dtype=np.float) * np.nan
+            read_ok_mask = np.ones((len(channels), len(start_offsets))).astype(bool)
+            for i, start_offset in enumerate(start_offsets):
+                if 'orient' in timeseries.attrs and timeseries.attrs['row'] == 'row':
+                    data = timeseries[start_offset:start_offset + read_size, channels_to_read].T
+                else:
+                    data = timeseries[channels_to_read, start_offset:start_offset + read_size]
+                if data.shape[-1] == read_size:
+                    eventdata[:, i, :] = data
+                else:
+                    print(
+                        'Cannot read full chunk of data for offset ' + str(start_offset) +
+                        'End of read interval  is outside the bounds of file ' + filename)
+                    read_ok_mask[:, i] = False
+            eegfile.close()
+
+        return eventdata, read_ok_mask
