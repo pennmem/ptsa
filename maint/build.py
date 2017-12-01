5f4b4a6bc27730c1859cb4d75d69c87a15684567
Michael V. DePalatis <mike@depalatis.net>
2017-12-01 12:51:50 -0500

    Update conda packaging

--- maint/build.py
+++ maint/build.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+
+from argparse import ArgumentParser
 import os
-from subprocess import Popen
 import shlex
+import shutil
+from subprocess import check_call
 
-py_versions = ["2.7", "3.5", "3.6"]
-cmd = "conda build --python {version:s} conda.recipe"
+parser = ArgumentParser()
+parser.add_argument('--no-clean', action='store_true', default=False,
+                    help='remove build directory')
+parser.add_argument('--no-build', action='store_true', default=False,
+                    help='do not build packages')
 
-for version in py_versions:
-    cmd_str = cmd.format(version=version)
 
-    env = os.environ.copy()
-    env["PYTHON_BUILD_VERSION"] = version
+def clean():
+    """Clean the build directory."""
+    try:
+        shutil.rmtree('build')
+        os.mkdir('build')
+    except OSError:
+        pass
 
-    print("Building for Python", version)
-    print(cmd_str)
-    Popen(shlex.split(cmd_str), env=env).wait()
+
+def build():
+    """Build conda packages."""
+    for pyver in ['2.7', '3.5', '3.6']:
+        build_cmd = "conda build conda.recipe --python {} --output-folder build/".format(pyver)
+        print(build_cmd)
+        check_call(shlex.split(build_cmd))
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if not args.no_clean:
+        clean()
+
+    if not args.no_build:
+        build()
