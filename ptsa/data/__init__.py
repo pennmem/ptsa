b9b8e80f6c8760a4f0a28213c1cacb634efd502a
Michael V. DePalatis <mike@depalatis.net>
2017-12-22 13:40:02 -0500

    Improve BaseRawReader documentation

--- ptsa/data/__init__.py
+++ ptsa/data/__init__.py
@@ -1,21 +1,5 @@
-# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
-# ex: set sts=4 ts=4 sw=4 et:
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-#
-#   See the COPYING file distributed along with the PTSA package for the
-#   copyright and license terms.
-#
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-
-from dimarray import Dim, DimArray, AttrArray
-from .timeseries import TimeSeries
+# FIXME: all of these is old PTSA stuff that should be removed
 
 from .basewrapper import BaseWrapper
 from .arraywrapper import ArrayWrapper
-
-try:
-    from .edfwrapper import EdfWrapper
-except ImportError:
-    print('Could not find compiled version of c library for handing edf files. If you want edf support, please run setup.py and compile this library')
-
 from .events import Events
