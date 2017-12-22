cc22af8c2ae72819cd0d908b5cb81bfca424c657
Michael V. DePalatis <mike@depalatis.net>
2017-12-22 15:03:12 -0500

    Quick hack to fix import errors on testing
    Need to more carefully remove old stuff.

--- ptsa/data/__init__.py
+++ ptsa/data/__init__.py
@@ -1,4 +1,6 @@
-# FIXME: all of these is old PTSA stuff that should be removed
+# FIXME: all of this is old PTSA stuff that should be removed
+TimeSeries = None
+Dim = None
 
 from .basewrapper import BaseWrapper
 from .arraywrapper import ArrayWrapper
