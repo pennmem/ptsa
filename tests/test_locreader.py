217d8e71a13bcdb9d136c2ec8177d3184975fd23
leond <lad258@cornell.edu>
2018-01-24 13:48:24 -0500

    added tests for LocReader

---
+++ tests/test_locreader.py
@@ -0,0 +1,22 @@
+from ptsa.data.readers import LocReader
+import os.path as osp
+import pandas as pd
+
+class TestLocReader:
+
+
+    def setup(cls):
+        loc_file = osp.join(osp.dirname(__file__), 'data', 'localization.json')
+        cls.localization =         LocReader(filename=loc_file).read()
+
+
+    def test_split(self):
+        contacts = self.localization.loc['contacts']
+        pairs =    self.localization.loc['pairs']
+
+    def test_types(self):
+        contacts = self.localization.loc['contacts']
+        assert pd.isnull(contacts['type']).sum() == 0
+
+        pairs = self.localization.loc['pairs']
+        assert pd.isnull(pairs['type']).sum() == 0
