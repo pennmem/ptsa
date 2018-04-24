252bdcb70ee5ccde17d3bad0cdc67ffdb6c86ae2
leond <lad258@cornell.edu>
2018-04-24 16:06:00 -0400

    slight refactor

--- tests/test_matlabio.py
+++ tests/test_matlabio.py
@@ -1,12 +1,16 @@
 from ptsa.data import MatlabIO
 import os.path as osp
+import pytest
 
-def test_read_matlab_struct():
-    filename = osp.join(osp.dirname(__file__),'data','test_events.mat')
+@pytest.fixture
+def filename():
+    return osp.join(osp.dirname(__file__),'data','test_events.mat')
+
+def test_read_matlab_struct(filename):
     events = MatlabIO.read_single_matlab_matrix_as_numpy_structured_array(filename,'events')
     assert len(events.squeeze()) == 191
     assert events.list.max() == 1
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     test_read_matlab_struct()
