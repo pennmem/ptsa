d1f7c489e58b64910a26f65c7d32a8b43d8a9273
leond <lad258@cornell.edu>
2018-04-25 10:59:24 -0400

    removed test case in filters.ButterworthFilter
    (cherry picked from commit a6ec540)

--- ptsa/data/filters/ButterworthFilter.py
+++ ptsa/data/filters/ButterworthFilter.py
@@ -1,8 +1,8 @@
 from numpy import asarray
 from xarray import DataArray
-from ptsa.filt import buttfilt
 from ptsa.data.timeseries import TimeSeries
 from ptsa.data.common import get_axis_index
+from ptsa.filt import buttfilt
 from ptsa.data.filters import BaseFilter
 import traits.api
 
@@ -64,77 +64,19 @@ class ButterworthFilter(BaseFilter):
 
 
 
-if __name__ == '__main__':
-    import numpy as np
-    from numpy.testing import *
-    from ptsa.data.readers import BaseEventReader
-    from ptsa.data.filters.morlet import MorletWaveletFilter
-    from ptsa.data.filters.ButterworthFilter import ButterworthFilter
-    from ptsa.data.readers.tal import TalReader
-    from ptsa.data.readers import EEGReader
-    from ptsa.data.readers import PTSAEventReader
-    from ptsa.data.events import Events
-
-    e_path = '/Volumes/rhino_root/data/events/RAM_PS/R1108J_1_events.mat'
-    e_path = '/Volumes/rhino_root/data/events/RAM_PS/R1108J_1_events.mat'
-    # e_path ='/Users/m/data/events/RAM_FR1/R1056M_events.mat'
-
-    e_reader = BaseEventReader(filename=e_path, eliminate_events_with_no_eeg=True)
+from scipy.signal import butter, lfilter
 
 
-    events = e_reader.read()
-
-    from ptsa.data.readers.tal import TalReader
-
-    tal_path = '/Volumes/rhino_root/data/eeg/R1108J_1/tal/R1108J_1_talLocs_database_bipol.mat'
-    tal_reader = TalReader(filename=tal_path)
-    monopolar_channels = tal_reader.get_monopolar_channels()
-    bipolar_pairs = tal_reader.get_bipolar_pairs()
-
-    # ---------------- NEW STYLE PTSA -------------------
-    base_e_reader = BaseEventReader(filename=e_path, eliminate_events_with_no_eeg=True)
-
-    base_events = base_e_reader.read()
-
-    base_events = base_events[(base_events.type == 'STIMULATING') ]
-    base_events = base_events[base_events.session == 2]
+def butter_bandpass(lowcut, highcut, fs, order=4):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    # b, a = butter(order, [low, high], btype='band')
+    b, a = butter(order, [low, high], btype='stop')
+    return b, a
 
 
-    from ptsa.data.readers.EEGReader import EEGReader
-    eeg_reader = EEGReader(events=base_events, channels=monopolar_channels[0:3],
-                           start_time=-1.1, end_time=-0.1, buffer_time=1.0)
-
-    base_eegs = eeg_reader.read()
-
-    bw_base_eegs = base_eegs.filtered(freq_range=[58.,62.], filt_type='stop', order=4)
-
-
-    b, a = butter_bandpass(58.,62.,float(base_eegs['samplerate']), 4)
-    y_in = base_eegs[0,0,:].data
-
-
-    y_out = butter_bandpass_filter(y_in, 58.,62.,float(base_eegs['samplerate']), 4)
-
-
-    import matplotlib;
-    matplotlib.use('Qt4Agg')
-
-
-    import matplotlib.pyplot as plt
-    plt.get_current_fig_manager().window.raise_()
-
-
-    # plt.plot(np.arange(y_in.shape[0]),y_in,'k')
-    plt.plot(np.arange(y_out.shape[0]),y_out,'r--')
-
-
-    plt.plot(np.arange(bw_base_eegs[0,0,:].shape[0]),bw_base_eegs[0,0,:],'b--')
-
-
-
-
-    plt.show()
-
-    print()
-
-
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
