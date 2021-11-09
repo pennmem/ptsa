3dcb5fd57e14d9a2c59c0c2c2f831f886cac99c2
Joseph Rudoler <40604402+jrudoler@users.noreply.github.com>
2021-11-09 02:24:23 -0500

    Revert "Patch for butterworth + MultiIndex"
    This reverts commit 841f5d4ac1a1ff8970a366ebaba857ccf9df2ee3.

--- ptsa/data/filters/butterworth.py
+++ ptsa/data/filters/butterworth.py
@@ -48,9 +48,18 @@ class ButterworthFilter(BaseFilter):
 
         """
         time_axis_index = get_axis_index(timeseries, axis_name='time')
-        filtered_array = buttfilt(timeseries.data,
+        filtered_array = buttfilt(timeseries,
                                   self.freq_range, float(timeseries['samplerate']), self.filt_type,
                                   self.order, axis=time_axis_index)
 
-        timeseries.data = filtered_array
-        return timeseries
+        coords_dict = {coord_name: DataArray(coord.copy()) for coord_name, coord in list(timeseries.coords.items())}
+        coords_dict['samplerate'] = timeseries['samplerate']
+        dims = [dim_name for dim_name in timeseries.dims]
+        filtered_timeseries = TimeSeries(
+            filtered_array,
+            dims=dims,
+            coords=coords_dict
+        )
+
+        filtered_timeseries.attrs = timeseries.attrs.copy()
+        return filtered_timeseries
