21871a3475b02d17775b31b81e5200945c139875
James Bruska <jamesbthecoder@gmail.com>
2022-12-20 17:12:13 -0500

    Made importlib-metadata use 4.13.0 because 5.0.0 and beyond remove get function that is needed for old xarray versions

--- maint/build.py
+++ maint/build.py
@@ -68,7 +68,7 @@ def convert():
 
 def upload():
     """ Upload pre-built package to conda """
-    for pltform in ['linux-64', 'osx-64', 'win-32', 'win-64']:
+    for pltform in ['linux-64', 'osx-64', 'osx-arm64', 'win-32', 'win-64']:
         files = glob.glob('build/{}/*.tar.bz2'.format(pltform))
         cmds = ['anaconda upload -u pennmem {}'.format(f) for f in files]
         for cmd in cmds:
